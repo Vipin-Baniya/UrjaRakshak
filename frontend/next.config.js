c86fa1e1ab983b8e0fcb03ed9696b674f16ce91f
Vipin-Baniya <Vips2004baniya@gmail.com>
2026-03-01 01:09:52 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   output: 'export',
   images: { unoptimized: true },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 }
